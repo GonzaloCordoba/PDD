12738bbaaa24dcabe80d1b22ba14ea0e2fce640e
gonzalo cordoba <gonzalo.cordoba@davinci.edu.ar>
2020-09-01 21:33:05 -0300

    nuevo cambio

---
+++ clase_01_09_20/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿    using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+                static void Main(string[] args)
+                {
+                    int chances = 0;
+                    int ingreso;
+
+                    Random random = new Random();
+                    int secreto = random.Next(10);
+
+                    do
+                    {
+                        Console.WriteLine("Adivene el numero:");
+                        ingreso = int.Parse(Console.ReadLine());
+                        chances++;
+                        if (ingreso == secreto)
+                        {
+                            Console.WriteLine("Ganaste!!");
+                            chances = 0;
+                        }
+                        else if(chances == 3)
+                        {
+                            Console.WriteLine("Perdiste! el numero secreto es:" + secreto);
+                    
+                        }
+
+                    } while (ingreso != secreto && chances <= 3);
+                }
+            }
+}
